f48c6e86e564e7091200d387292b2bbfd6a415e0
Thanh Pham <thanhagar123@gmail.com>
2026-04-19 17:28:39 +0700

    first commit

---
+++ backend/src/main/java/com/thanhpham/product_idea_validator/ProductIdeaValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.thanhpham.product_idea_validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductIdeaValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductIdeaValidatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thanhpham/product_idea_validator/ProductIdeaValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhpham.product_idea_validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductIdeaValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
