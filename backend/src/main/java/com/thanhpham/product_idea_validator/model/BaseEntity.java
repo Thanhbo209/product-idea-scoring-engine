ce80c694391a3161ec18f4211f47b0e5b584f5b6
Thanh Pham <thanhagar123@gmail.com>
2026-04-20 14:04:29 +0700

    feat(auth): implement auth service for login/register

--- backend/src/main/java/com/thanhpham/product_idea_validator/model/BaseEntity.java
+++ backend/src/main/java/com/thanhpham/product_idea_validator/model/BaseEntity.java
@@ -8,9 +8,11 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.Setter;
 
 @Setter
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
