9e5a3ecc0961e72fd9ce69f1000e4f8e03ae3b66
Thanh Pham <thanhagar123@gmail.com>
2026-04-23 14:52:04 +0700

    fix(ci): setup env and ci bug

--- backend/src/test/java/com/thanhpham/product_idea_validator/ProductIdeaValidatorApplicationTests.java
+++ backend/src/test/java/com/thanhpham/product_idea_validator/ProductIdeaValidatorApplicationTests.java
@@ -2,8 +2,10 @@ package com.thanhpham.product_idea_validator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProductIdeaValidatorApplicationTests {
 
 	@Test
